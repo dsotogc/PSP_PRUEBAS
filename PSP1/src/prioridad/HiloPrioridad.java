51cdb7d19e88fe8502554cd4471478e8102468e9
DAVID SOTO GARCÍA <david.sotgar.1@educa.jcyl.es>
2025-11-06 09:19:37 +0100

    prioridad solucionado

---
+++ PSP1/src/prioridad/HiloPrioridad.java
@@ -0,0 +1,43 @@
+package prioridad;
+
+public class HiloPrioridad extends Thread {
+	
+	private int cont = 0;
+	private boolean stop = false;
+	
+	public int getContador()
+	{
+		return cont;
+	}
+	
+	public boolean getStop()
+	{
+		return stop;
+	}
+	
+	public void setContador(int cont)
+	{
+		this.cont = cont;
+	}
+	
+	public void setStop(boolean stop)
+	{
+		this.stop = stop;
+	}
+	
+	public void pararHilo()
+	{
+		setStop(true);
+	}
+	
+	public void run()
+	{
+		while (!this.stop)
+		{
+			cont++;
+			System.out.println("a");
+		}
+		System.out.println("Terminado " + this.getName());
+	}
+	
+}
---
+++ PSP1/src/prioridad/MainPrioridad.java
@@ -0,0 +1,41 @@
+package prioridad;
+
+public class MainPrioridad {
+
+	public static void main(String[] args) {
+		
+		HiloPrioridad h1 = new HiloPrioridad();
+		HiloPrioridad h2 = new HiloPrioridad();
+		HiloPrioridad h3 = new HiloPrioridad();
+		
+		h1.setPriority(Thread.MAX_PRIORITY);
+		h3.setPriority(Thread.MIN_PRIORITY);
+		
+		h1.start();
+		h2.start();
+		h3.start();
+		
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		h1.pararHilo();
+		h2.pararHilo();
+		h3.pararHilo();
+		
+		try {
+			h1.join();
+			h2.join();
+			h3.join();
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(h1.getStop());
+		System.out.printf("H1: %d Prio: %d\nH2: %d Prio: %d\nH3: %d Prio: %d", h1.getContador(), h1.getPriority(), h2.getContador(), h2.getPriority(), h3.getContador(), h3.getPriority());
+
+	}
+
+}
